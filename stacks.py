7748aefbe6c3d5440d0d9e02ab7020653481ad66
Dominique Cuevas <cuevasdominique@gmail.com>
2020-01-02 15:36:26 -0800

    parens_balanced using a stack

--- stacks.py
+++ stacks.py
@@ -3,7 +3,7 @@ class Stack:
         push
         pop
         peek
-        isEmpty
+        is_empty
         size
 
         Chose a built-in python list as data structure for items attribute and the end
@@ -11,7 +11,7 @@ class Stack:
         push is O(1) constant time
         pop is O(1) for removing from the end of a python list
         peek is O(1) for indexing
-        isEmpty is O(1) for checking if the list is empty
+        is_empty is O(1) for checking if the list is empty
         size is O(1) since native lists 
         If the beginning of the list was the top of the stack, pop would be O(n) 
         and push would also be O(n) and not the best runtime
@@ -29,7 +29,7 @@ class Stack:
     def peek(self):
         return self.items[-1]
 
-    def isEmpty(self):
+    def is_empty(self):
         return self.items == []
 
     def size(self):
@@ -44,9 +44,30 @@ def revstring(string):
     for letter in string:
         s.push(letter)
     reversedstr = ""
-    while not s.isEmpty():
+    while not s.is_empty():
         reversedstr += s.pop()
     return reversedstr
+
+def parens_balanced(string):
+    """are parenthese balanced
+        use a stack to easily keep track of parentheses
+        loop through string
+        add a beginning parentheses to stack
+        when there is a closing parentheses,
+            pop one of the open parentheses in stack
+        the stack should be empty if parenthese are balanced
+    """
+    parens_stack = Stack()
+
+    for char in string:
+        if char == "(":
+            parens_stack.push(char)
+        elif char == ")":
+            if parens_stack.is_empty(): # check for matching begin "("
+                return False            # fail quick and fall out of loop if failed to match
+            else:
+                parens_stack.pop()
+    return parens_stack.is_empty()      # stack should be empty if balanced
 
 if __name__ == "__main__":
     print(revstring('Nikki'))
