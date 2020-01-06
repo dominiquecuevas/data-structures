fae093cdc8212321b1e48b2889b04b6a8f6a4f89
Dominique Cuevas <cuevasdominique@gmail.com>
2020-01-06 11:41:58 -0800

    find method

--- tree.py
+++ tree.py
@@ -6,9 +6,18 @@ class Node:
     def __repr__(self):
         return f"<Node data ={self.data}>"
 
+    def find(self, data):
+        to_visit = [self]                       # start at node
+        while to_visit:                         # loop while there are still nodes
+            current = to_visit.pop()            # set current to last item (DFS)
+            if current.data == data:
+                return current
+            to_visit.extend(current.children)   # add to search list
+
 class Tree:
     def __init__(self, root):
         self.root = root
 
     def __repr__(self):
         return f"<Tree root={self.root}>"
+
