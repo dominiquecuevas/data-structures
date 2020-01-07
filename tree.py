dfe00497fb2a172a44c68b2de034a680ece9d0d7
Dominique Cuevas <cuevasdominique@gmail.com>
2020-01-07 15:46:20 -0800

    find method recursively

--- tree.py
+++ tree.py
@@ -4,7 +4,7 @@ class Node:
         self.children = children
 
     def __repr__(self):
-        return f"<Node data ={self.data}>"
+        return f"<Node data={self.data}>"
 
     def find(self, data):
         to_visit = [self]                       # start at node
@@ -14,6 +14,17 @@ class Node:
                 return current
             to_visit.extend(current.children)   # add to search list
 
+    def find_recursively(self, data, to_visit = []):
+        if self.data == data:
+            print(self)
+            return
+        to_visit.extend(self.children)
+        if not to_visit:
+            print("Not found")
+            return
+        current = to_visit.pop()
+        current.find_recursively(data, to_visit)
+
 class Tree:
     def __init__(self, root):
         self.root = root
@@ -21,3 +32,19 @@ class Tree:
     def __repr__(self):
         return f"<Tree root={self.root}>"
 
+dumby = Node("Dumbledore", [])
+snape = Node("Snape",[])
+flitwick = Node("Flitwick", [])
+malfoy = Node("Malfoy", [])
+mcgonagall = Node("McGonagall", [])
+ron = Node("Ron", [])
+hermoine = Node("Hermoine", [])
+crabbe = Node("Crabbe", [])
+dumby.children.append(snape)
+dumby.children.append(flitwick)
+snape.children.append(malfoy)
+malfoy.children.append(crabbe)
+dumby.children.append(mcgonagall)
+mcgonagall.children.append(ron)
+mcgonagall.children.append(hermoine)
+tree = Tree(dumby)
